b5a456c374d727f1b88544cbcf137ae20681d68c
Jeon. <86655940+NOESYU@users.noreply.github.com>
2024-10-23 14:54:43 +0900

    이롬 헤더

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(".submenu").hide();
+$(".menu>li").mouseover(function(){
+  $(this).children(".submenu").stop().slideDown();
+})
+$(".menu>li").mouseout(function(){
+  $(this).children(".submenu").stop().slideUp();
+})
